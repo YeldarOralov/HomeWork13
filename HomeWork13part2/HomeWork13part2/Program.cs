47bc9ca9e5c3aebdb0a8fe6eb2c10df57d474cf9
Eldar Oralov <eldar.oralov@gmail.com>
2019-03-20 18:04:06 +0600

    Добавление дз: запись класса студент в хмл

---
+++ HomeWork13part2/HomeWork13part2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HomeWork13part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student
+            {
+                FullName = "Касым-Жомарт Токаев",
+                Age = 65,
+                Country = "Kazakhstan",
+                University = "MGIMO USSR"
+            };
+
+            XmlDocument xml = new XmlDocument();
+            XmlElement root = xml.CreateElement("student");
+            XmlElement fullname = xml.CreateElement("fullname");
+            XmlElement age = xml.CreateElement("age");
+            XmlElement country = xml.CreateElement("country");
+            XmlElement university = xml.CreateElement("university");
+            fullname.InnerText = student.FullName;
+            age.InnerText = student.Age.ToString();
+            country.InnerText = student.Country;
+            university.InnerText = student.University;
+
+            root.AppendChild(fullname);
+            root.AppendChild(age);
+            root.AppendChild(country);
+            root.AppendChild(university);
+
+            xml.AppendChild(root);
+            xml.Save("student.xml");
+            
+        }
+    }
+}
---
+++ HomeWork13part2/HomeWork13part2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork13part2
+{
+    public class Student
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string University { get; set; }
+    }
+}
